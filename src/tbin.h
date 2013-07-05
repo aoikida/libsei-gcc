ab91de924879aed000e12153ab1ca49b1d0530d4
db7 <db7@demux.eu>
2013-07-05 15:53:16 +0200

    tbin module

---
+++ src/tbin.h
@@ -0,0 +1,17 @@
+/* -----------------------------------------------------------------------------
+ * Copyright (c) 2013 Diogo Behrens
+ * Distributed under the MIT license. See accompanying file LICENSE.
+ * -------------------------------------------------------------------------- */
+#ifndef _ASCO_TBIN_H_
+#define _ASCO_TBIN_H_
+
+#include <stdint.h>
+
+typedef struct tbin tbin_t;
+
+tbin_t* tbin_init(int max_items);
+void    tbin_fini(tbin_t* tbin);
+void    tbin_add(tbin_t* tbin, void* ptr, int p);
+void    tbin_flush(tbin_t* tbin);
+
+#endif /* _ASCO_TBIN_H_ */
