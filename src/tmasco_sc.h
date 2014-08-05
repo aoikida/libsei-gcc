938a268d180c2539caf58c4ef1e7c7934c69c3ed
sergey <>
2014-08-05 16:10:26 +0200

    Header file with typedefs

---
+++ src/tmasco_sc.h
@@ -0,0 +1,23 @@
+/*
+ * tmasco_sc.h
+ *
+ *  Created on: Aug 4, 2014
+ *      Author: sergey
+ */
+
+#ifndef TMASCO_SC_H_
+#define TMASCO_SC_H_
+
+/* Typedefs for wrapped system calls */
+
+#include <sys/socket.h>
+
+typedef int (socket_f)(int domain, int type, int protocol);
+typedef int (close_f)(int fd);
+typedef int (bind_f)(int sockfd, const struct sockaddr *addr,
+		socklen_t addrlen);
+typedef int (connect_f)(int socket, const struct sockaddr *addr, socklen_t length);
+typedef ssize_t (send_f)(int socket, const void *buffer, size_t size, int flags);
+
+
+#endif /* TMASCO_SC_H_ */
