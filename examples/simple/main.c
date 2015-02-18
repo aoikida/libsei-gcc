7e7ffd753201874a64fcad355084e2e7d408d9b2
sergey <>
2015-02-18 17:40:39 +0100

    retab simple

--- examples/simple/main.c
+++ examples/simple/main.c
@@ -1,7 +1,7 @@
-/* ----------------------------------------------------------------------
+/* ----------------------------------------------------------------------------
  * Copyright (c) 2015 Diogo Behrens, Sergei Arnautov
  * Distributed under the MIT license. See accompanying file LICENSE.
- * ------------------------------------------------------------------- */
+ * ------------------------------------------------------------------------- */
 
 #include <sei.h>
 #include <stdio.h>
@@ -29,117 +29,118 @@ size_t strlen(const char *s) SEI_PURE;
 
 /* initialize the state */
 void init_counter() {
-	counter = 0;
+    counter = 0;
 }
 
 /* extract crc from the message
  * to simplify the example we read input messages from a file */
 size_t 
 recv_msg_and_crc(char** imsg, uint32_t* crc) {
-	if (ifile == NULL) {
-		ifile = fopen("input", "r");
+    if (ifile == NULL) {
+        ifile = fopen("input", "r");
 
-		if (ifile == NULL) {
-			printf("could not open input file\n");
-			exit(-1);
-		}
-	}
+        if (ifile == NULL) {
+            printf("could not open input file\n");
+            exit(-1);
+        }
+    }
 
-	size_t n;
-	ssize_t read = getline(&buffer, &n, ifile);
+    size_t n;
+    ssize_t read = getline(&buffer, &n, ifile);
 
-	if (read < 0) {
-		fclose(ifile);
-		if (ofile)
-			fclose(ofile);
-		exit(0);
-	}
+    if (read < 0) {
+        fclose(ifile);
+        if (ofile)
+            fclose(ofile);
+        exit(0);
+    }
 
-	*crc  = *(uint32_t*)buffer;
-	*imsg = buffer + sizeof(*crc);
-	return read - sizeof(*crc);
+    *crc  = *(uint32_t*)buffer;
+    *imsg = buffer + sizeof(*crc);
+    return read - sizeof(*crc);
 }
 
 /* process the input message: simply add message size to the counter */
 void
 do_something_here(const char* imsg) {
-	counter += strlen(imsg);
+    counter += strlen(imsg);
 }
 
 /* create output message: counter value */
 char*
 create_a_message_here(size_t* olen) {
-	char* msg = malloc(16);
-	sprintf(msg, "counter=%zu\n", counter);
-	*olen = strlen(msg);
-	return msg;
+    char* msg = malloc(16);
+    sprintf(msg, "counter=%zu\n", counter);
+    *olen = strlen(msg);
+    return msg;
 }
 
 /* send the message along with crc (write it to a file in this example) */
 void
 send_msg_and_crc(char* omsg, const size_t olen, const uint32_t crc) {
-	if (ofile == NULL) {
-		ofile = fopen("output", "w");
+    if (ofile == NULL) {
+        ofile = fopen("output", "w");
 
-		if (ofile == NULL) {
-			if (ifile)
-				fclose(ifile);
-			printf("cannot open output file\n");
-			exit(-1);
-		}
-	}
+        if (ofile == NULL) {
+            if (ifile)
+                fclose(ifile);
+            printf("cannot open output file\n");
+            exit(-1);
+        }
+    }
 
-	fwrite(&crc, sizeof(crc), 1, ofile); 
-	fwrite(omsg, olen, 1, ofile);
+    fwrite(&crc, sizeof(crc), 1, ofile); 
+    fwrite(omsg, olen, 1, ofile);
 
-	free(omsg);
-	free(buffer);
-	buffer = NULL;
+    free(omsg);
+    free(buffer);
+    buffer = NULL;
 }
 
 int 
 main(int argc, char** args) {
-	char     *imsg = NULL, *omsg = NULL;
-	uint32_t crc;
-	size_t   ilen, olen;
+    char     *imsg = NULL, *omsg = NULL;
+    uint32_t crc;
+    size_t   ilen, olen;
 
-	/* initialize the state using a hardened event handler which will be
-	 * executed twice. Since no message is received, use __begin_nm - no message */
-	__begin_nm();
-	init_counter();
-	__end_nm();
+    /* initialize the state using a hardened event handler which will be
+     * executed twice. Since no message is received, use __begin_nm - no
+     * message */
+    __begin_nm();
+    init_counter();
+    __end_nm();
 
-	while(1) {
-		ilen = recv_msg_and_crc(&imsg, &crc);
+    while(1) {
+        ilen = recv_msg_and_crc(&imsg, &crc);
 
-		/* __begin checks if incoming message is correct
-		 * must be called within if-statement   */
-		if (__begin(imsg, ilen, crc)) { 
+        /* __begin checks if incoming message is correct
+         * must be called within if-statement   */
+        if (__begin(imsg, ilen, crc)) { 
 
-			/* process the message */
-			do_something_here(imsg); 
+            /* process the message */
+            do_something_here(imsg); 
 
-			/* create output message */
-			omsg = create_a_message_here(&olen);
+            /* create output message */
+            omsg = create_a_message_here(&olen);
 
-			/* calculate CRC of the output message; if only a part of the 
-			 * message is created, this function should be called again 
-			 * for other parts of the message */
-			__output_append(omsg, olen); 
+            /* calculate CRC of the output message; if only a part of the 
+             * message is created, this function should be called again 
+             * for other parts of the message */
+            __output_append(omsg, olen); 
 
-			/* Finalize CRC once a complete output message was created */
-			__output_done(); 
+            /* Finalize CRC once a complete output message was created */
+            __output_done(); 
 
-			/* end of the hardened event handler */
-			__end();
-		} else /* discard invalid input */ 
-			continue; 
-		
-		printf("counter: %zu\n", counter);
-		
-		/* read the calculated CRC and send it along with the message */
-		send_msg_and_crc(omsg, olen, __crc_pop());
-	}
+            /* end of the hardened event handler */
+            __end();
+        } else /* discard invalid input */ 
+            continue; 
+        
+        printf("counter: %zu\n", counter);
+        
+        /* read the calculated CRC and send it along with the message */
+        send_msg_and_crc(omsg, olen, __crc_pop());
+    }
 
-	return 0;
+    return 0;
 }
