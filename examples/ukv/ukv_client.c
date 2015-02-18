df80a34edab2c20b1ec6ff49d3b61ebcd19ad615
sergey <>
2015-02-18 17:37:45 +0100

    retab ukv

--- examples/ukv/ukv_client.c
+++ examples/ukv/ukv_client.c
@@ -1,7 +1,7 @@
-/* -----------------------------------------------------------------------------
+/* ----------------------------------------------------------------------------
  * Copyright (c) 2015 Diogo Behrens, Sergei Arnautov
  * Distributed under the MIT license. See accompanying file LICENSE.
- * -------------------------------------------------------------------------- */
+ * ------------------------------------------------------------------------- */
 
 #include <string.h>
 #include <stdlib.h>
@@ -17,88 +17,88 @@ int
 main(const int argc, const char* argv[])
 {
 
-	if (argc < 3) {
-		printf("usage: %s <server ip> <server port>\n", argv[0]);
-		return EXIT_FAILURE;
-	}
+    if (argc < 3) {
+        printf("usage: %s <server ip> <server port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-	int fd;
-	struct sockaddr_in addr;
-	int port;
-	char* req = NULL;
-	ssize_t read;
-	char buffer[BUFSIZE+1];
-	size_t l = 0;
+    int fd;
+    struct sockaddr_in addr;
+    int port;
+    char* req = NULL;
+    ssize_t read;
+    char buffer[BUFSIZE+1];
+    size_t l = 0;
 
-	port = atoi(argv[2]);
+    port = atoi(argv[2]);
 
-	// create socket and connect
-	fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (fd < 0) {
-		perror("failed to create a socket");
-		return EXIT_FAILURE;
-	}
+    // create socket and connect
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        perror("failed to create a socket");
+        return EXIT_FAILURE;
+    }
 
-	bzero(&addr, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = inet_addr(argv[1]);
-	addr.sin_port = htons(port);
-	
-	if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-		perror("connect failed");
-		return EXIT_FAILURE;
-	}
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    addr.sin_port = htons(port);
+    
+    if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("connect failed");
+        return EXIT_FAILURE;
+    }
 
-	printf("ukv interface:\n\tset: +key,value\n\tget: ?key\n\tdel: -key\n\n");
-	printf("type exit to quit\n\n");
+    printf("ukv interface:\n\tset: +key,value\n\tget: ?key\n\tdel: -key\n\n");
+    printf("type exit to quit\n\n");
 
-	printf("enter command: \n");
+    printf("enter command: \n");
 
-	while ((read = getline(&req, &l, stdin)) != -1) {
-		if (read > 1) { 
-			if (strcmp(req, strdup("exit\n")) == 0) {
-				free(req);
-				exit(0);
-			}	
-			// the server expects \r\n in the end of the request
-			// overwrite \n in the end and the terminating null
-			req[read-1] = '\r';
-			req[read]   = '\n';
-			read++;
+    while ((read = getline(&req, &l, stdin)) != -1) {
+        if (read > 1) { 
+            if (strcmp(req, strdup("exit\n")) == 0) {
+                free(req);
+                exit(0);
+            }   
+            // the server expects \r\n in the end of the request
+            // overwrite \n in the end and the terminating null
+            req[read-1] = '\r';
+            req[read]   = '\n';
+            read++;
 
-			// calculate CRC of the request
-			uint32_t crc = crc_compute(req, read);
-			 
-			// preprend the request with CRC and send
-			ssize_t len = read + sizeof(crc);
-			char buf[len];
-			memcpy(buf, &crc, sizeof(crc));
-			memcpy(buf + sizeof(crc), req, read);
-			sendto(fd,buf,len,0,(struct sockaddr *)&addr,sizeof(addr)); 
+            // calculate CRC of the request
+            uint32_t crc = crc_compute(req, read);
+             
+            // preprend the request with CRC and send
+            ssize_t len = read + sizeof(crc);
+            char buf[len];
+            memcpy(buf, &crc, sizeof(crc));
+            memcpy(buf + sizeof(crc), req, read);
+            sendto(fd,buf,len,0,(struct sockaddr *)&addr,sizeof(addr)); 
 
-			// get response and check CRC
-			len = recv(fd, buffer, BUFSIZE, 0);
-			if (len < 0) {
-				perror("receive failed");
-			 	return EXIT_FAILURE;
-			}
+            // get response and check CRC
+            len = recv(fd, buffer, BUFSIZE, 0);
+            if (len < 0) {
+                perror("receive failed");
+                return EXIT_FAILURE;
+            }
 
-			buffer[len] = '\0';
+            buffer[len] = '\0';
 
-			uint32_t icrc = *(uint32_t*)buffer;
-			char* msg = buffer + sizeof(icrc);
-			crc = crc_compute(msg, len-sizeof(icrc));
+            uint32_t icrc = *(uint32_t*)buffer;
+            char* msg = buffer + sizeof(icrc);
+            crc = crc_compute(msg, len-sizeof(icrc));
 
-			if (icrc != crc) 
-				printf("dropping corrupted response\n");
-			else 
-				printf("server response: %s", msg);
+            if (icrc != crc) 
+                printf("dropping corrupted response\n");
+            else 
+                printf("server response: %s", msg);
 
-			printf("------------------------\n");
+            printf("------------------------\n");
 
-			free(req);
-			req = NULL;
-		}
-	}
-	return 0;
+            free(req);
+            req = NULL;
+        }
+    }
+    return 0;
 }
--- examples/ukv/ukv_server.c
+++ examples/ukv/ukv_server.c
@@ -1,7 +1,7 @@
-/* -----------------------------------------------------------------------------
- * Copyright (c) 2013 Diogo Behrens, Sergei Arnautov
+/* ----------------------------------------------------------------------------
+ * Copyright (c) 2013 Diogo Behrens, Sergei Arnautov 
  * Distributed under the MIT license. See accompanying file LICENSE.
- * -------------------------------------------------------------------------- */
+ * ------------------------------------------------------------------------- */
 
 #include <sei.h>
 #include <string.h>
@@ -22,201 +22,202 @@ enum {INIT, ACCT, RECV, PROC, SEND, FINI, CORR} state;
 int
 main(const int argc, const char* argv[])
 {
-	if (argc < 2) {
-		printf("usage: %s <port>\n", argv[0]);
-		return EXIT_FAILURE;
-	}
+    if (argc < 2) {
+        printf("usage: %s <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-	int fd, cfd, port;
-	uint32_t crc; 
-	struct sockaddr_in addr, caddr;
-	ssize_t read, msg_len;
-	socklen_t len;
-	char buffer[BUFSIZE+1];
-	char*  msg;
-	const char* r;
-	ukv_t* ukv;
+    int fd, cfd, port;
+    uint32_t crc; 
+    struct sockaddr_in addr, caddr;
+    ssize_t read, msg_len;
+    socklen_t len;
+    char buffer[BUFSIZE+1];
+    char*  msg;
+    const char* r;
+    ukv_t* ukv;
 
-	len   = sizeof(caddr);
-	port  = atoi(argv[1]);
-	state = INIT;
+    len   = sizeof(caddr);
+    port  = atoi(argv[1]);
+    state = INIT;
 
-	while(1) {
-		switch(state) {
-			case INIT: {
-				// create socket, bind and listen
-				fd = socket(AF_INET, SOCK_STREAM, 0);
-				if (fd < 0) {
-					perror("socket");
-					return EXIT_FAILURE;
-				}
+    while(1) {
+        switch(state) {
+            case INIT: {
+                // create socket, bind and listen
+                fd = socket(AF_INET, SOCK_STREAM, 0);
+                if (fd < 0) {
+                    perror("socket");
+                    return EXIT_FAILURE;
+                }
 
-				// this is important, so that if a process restarts, it can
-				// quickly reuse the same port
-				int on = 1;
-				if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
-					perror("setsockopt");
-					return EXIT_FAILURE;
-				}
+                // this is important, so that if a process restarts, it can
+                // quickly reuse the same port
+                int on = 1;
+                if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &on, 
+                               sizeof(on)) < 0) {
+                    perror("setsockopt");
+                    return EXIT_FAILURE;
+                }
 
-				bzero(&addr, sizeof(addr));
-				addr.sin_family      = AF_INET;
-				addr.sin_addr.s_addr = htonl(INADDR_ANY);
-				addr.sin_port        = htons(port);
+                bzero(&addr, sizeof(addr));
+                addr.sin_family      = AF_INET;
+                addr.sin_addr.s_addr = htonl(INADDR_ANY);
+                addr.sin_port        = htons(port);
 
-				if (bind(fd, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
-					perror("bind");
-					return EXIT_FAILURE;
-				}
+                if (bind(fd, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+                    perror("bind");
+                    return EXIT_FAILURE;
+                }
 
-				if (listen(fd, 2) < 0) {
-					perror("listen");
-					return EXIT_FAILURE;
-				}
+                if (listen(fd, 2) < 0) {
+                    perror("listen");
+                    return EXIT_FAILURE;
+                }
 
-				// initialize ukv service
-				__begin_nm();
-				ukv = ukv_init();
-				__end_nm();
+                // initialize ukv service
+                __begin_nm();
+                ukv = ukv_init();
+                __end_nm();
 
-				state = ACCT;
-				break;
-			}
-			case ACCT: 
-        		// accept client connections
-				cfd = accept(fd, (struct sockaddr*) &caddr, &len);
-				if (cfd < 0) {
-					perror("accept");
-					break;
-				}
+                state = ACCT;
+                break;
+            }
+            case ACCT: 
+                // accept client connections
+                cfd = accept(fd, (struct sockaddr*) &caddr, &len);
+                if (cfd < 0) {
+                    perror("accept");
+                    break;
+                }
 
-				state = RECV;
-				break;
-			case RECV: {
-				// once a connection is accepted, read from the connection
-				// until it is closed
-				read = recvfrom(cfd, buffer, BUFSIZE, 0,
-			   	                (struct sockaddr*) &caddr, &len);
-				if (read <= 0) {
-					perror("recvfrom");
-					close(cfd);
+                state = RECV;
+                break;
+            case RECV: {
+                // once a connection is accepted, read from the connection
+                // until it is closed
+                read = recvfrom(cfd, buffer, BUFSIZE, 0,
+                                (struct sockaddr*) &caddr, &len);
+                if (read <= 0) {
+                    perror("recvfrom");
+                    close(cfd);
 
-					state = ACCT;
-					break;
-				}
+                    state = ACCT;
+                    break;
+                }
 
-				buffer[read] = '\0';
-				
-				// we assume that we received the whole message. This is
-				// definitely not correct.
-				
+                buffer[read] = '\0';
+                
+                // we assume that we received the whole message. This is
+                // definitely not correct.
+                
 #ifdef SEI_ENABLED
-				// assume first four bytes are CRC of the request
-				crc     = *(uint32_t*)buffer;
-				msg     = buffer + sizeof(crc);
-				msg_len = read - sizeof(crc);
+                // assume first four bytes are CRC of the request
+                crc     = *(uint32_t*)buffer;
+                msg     = buffer + sizeof(crc);
+                msg_len = read - sizeof(crc);
 #else
-				msg = buffer;
+                msg = buffer;
 #endif
-				printf("received: %s", msg);
+                printf("received: %s", msg);
 
-				state = PROC;
-				break;
-			}
-			case PROC: {
+                state = PROC;
+                break;
+            }
+            case PROC: {
 
-				r = NULL;
-				// check CRC of the incoming message
-				if (__begin(msg, msg_len, crc)) {
-					r =  ukv_recv(ukv, msg);
+                r = NULL;
+                // check CRC of the incoming message
+                if (__begin(msg, msg_len, crc)) {
+                    r =  ukv_recv(ukv, msg);
 
-					if (!r) 
-						state = FINI;
-					else {
-						// calculate CRC of the response message
-						__output_append(r, strlen(r));
+                    if (!r) 
+                        state = FINI;
+                    else {
+                        // calculate CRC of the response message
+                        __output_append(r, strlen(r));
 
-						// since the message is complete, finalize CRC
-						__output_done();
+                        // since the message is complete, finalize CRC
+                        __output_done();
 
-						state = SEND;
-					}
+                        state = SEND;
+                    }
 
-					__end();
-				} else {
-					// the message is corrupted, drop it
-					state = CORR;
-				}
+                    __end();
+                } else {
+                    // the message is corrupted, drop it
+                    state = CORR;
+                }
 
-				break;
-			}
-			case SEND: {
+                break;
+            }
+            case SEND: {
 #ifdef SEI_ENABLED
-				// read the calculated CRC and add it to the response
-				uint32_t ocrc =  __crc_pop();
-				msg_len = sizeof(uint32_t) + strlen(r);
-				char response[msg_len];
+                // read the calculated CRC and add it to the response
+                uint32_t ocrc =  __crc_pop();
+                msg_len = sizeof(uint32_t) + strlen(r);
+                char response[msg_len];
 
-				memcpy(response, &ocrc, sizeof(int));
-				memcpy(response + sizeof(int), r, strlen(r));
-				sendto(cfd, response, msg_len, 0,
-				       (struct sockaddr*) &caddr, sizeof(caddr));
+                memcpy(response, &ocrc, sizeof(int));
+                memcpy(response + sizeof(int), r, strlen(r));
+                sendto(cfd, response, msg_len, 0,
+                       (struct sockaddr*) &caddr, sizeof(caddr));
 #else
-				sendto(cfd, r, strlen(r), 0,
-				       (struct sockaddr*) &caddr, sizeof(caddr));
+                sendto(cfd, r, strlen(r), 0,
+                       (struct sockaddr*) &caddr, sizeof(caddr));
 #endif
-				printf("replied: %s", r);
+                printf("replied: %s", r);
 
-				__begin_nm();
-				ukv_done(ukv, r);
-				__end_nm();
-				printf("---------------------\n");
-				
-				state = RECV;
-				break;
-			}
-			case CORR: {
-				// if we received a corrupted message, send a response to the
-				// client anyway
-				char* reply;
-				__begin_nm();
-				reply = strdup("%error\r\n");
-				if (!reply)
-					return EXIT_FAILURE;
+                __begin_nm();
+                ukv_done(ukv, r);
+                __end_nm();
+                printf("---------------------\n");
+                
+                state = RECV;
+                break;
+            }
+            case CORR: {
+                // if we received a corrupted message, send a response to the
+                // client anyway
+                char* reply;
+                __begin_nm();
+                reply = strdup("%error\r\n");
+                if (!reply)
+                    return EXIT_FAILURE;
 
-				__output_append(reply, strlen(reply));
-				__output_done();
+                __output_append(reply, strlen(reply));
+                __output_done();
 
-				__end_nm();
-				printf("replied: %s", reply);
-				printf("---------------------\n");
+                __end_nm();
+                printf("replied: %s", reply);
+                printf("---------------------\n");
 
 #ifdef SEI_ENABLED
-				uint32_t ocrc =  __crc_pop();
-				msg_len = sizeof(uint32_t) + strlen(reply);
-				char response[msg_len];
+                uint32_t ocrc =  __crc_pop();
+                msg_len = sizeof(uint32_t) + strlen(reply);
+                char response[msg_len];
 
-				memcpy(response, &ocrc, sizeof(int));
-				memcpy(response + sizeof(int), reply, strlen(reply));
-				sendto(cfd, response, msg_len, 0,
-					   (struct sockaddr*) &caddr, sizeof(caddr));
+                memcpy(response, &ocrc, sizeof(int));
+                memcpy(response + sizeof(int), reply, strlen(reply));
+                sendto(cfd, response, msg_len, 0,
+                       (struct sockaddr*) &caddr, sizeof(caddr));
 #else
-				sendto(cfd, reply, strlen(reply), 0,
-				       (struct sockaddr*) &caddr, sizeof(caddr));
+                sendto(cfd, reply, strlen(reply), 0,
+                       (struct sockaddr*) &caddr, sizeof(caddr));
 #endif
 
-				free(reply);
+                free(reply);
 
-				state = RECV;
-				break;
-			}
-			case FINI: 
-				close(fd);
-				__begin_nm();
-				ukv_fini(ukv);
-				__end_nm();
+                state = RECV;
+                break;
+            }
+            case FINI: 
+                close(fd);
+                __begin_nm();
+                ukv_fini(ukv);
+                __end_nm();
 
-				return 0;
-		}
-	}
+                return 0;
+        }
+    }
 }
