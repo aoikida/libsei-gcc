d0793621c6f1bf27fff3c61a393e1596a6440a0b
db7 <db7@demux.eu>
2015-03-14 12:46:23 +0100

    line numbers in debug mode

--- src/debug.h
+++ src/debug.h
@@ -7,19 +7,32 @@
 #include <stdio.h>
 
 #if DEBUG >= 1
-#define DLOG1(...) printf(__VA_ARGS__)
+#define DLOG1(...)                                          \
+    do {                                                    \
+        fprintf(stderr, "[%s:%d](1) ", __FILE__, __LINE__); \
+        fprintf(stderr, __VA_ARGS__);                       \
+    } while (0);
 #else
 #define DLOG1(...)
 #endif
 
 #if DEBUG >= 2
-#define DLOG2(...) printf(__VA_ARGS__)
+#define DLOG2(...)                                          \
+    do {                                                    \
+        fprintf(stderr, "[%s:%d](2) ", __FILE__, __LINE__); \
+        fprintf(stderr, __VA_ARGS__);                       \
+    } while (0);
 #else
 #define DLOG2(...)
 #endif
 
 #if DEBUG >= 3
-#define DLOG3(...) printf(__VA_ARGS__)
+#define DLOG3(...)                                          \
+    do {                                                    \
+        fprintf(stderr, "[%s:%d](3) ", __FILE__, __LINE__); \
+        fprintf(stderr, __VA_ARGS__);                       \
+    } while (0);
+
 #else
 #define DLOG3(...)
 #endif
