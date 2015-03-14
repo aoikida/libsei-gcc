bd3f5f59799e1df31d1a0e8112bf5583dd738bbd
db7 <db7@demux.eu>
2015-03-14 13:00:11 +0100

    automatic reallocation of abuf and cow

--- src/config.h
+++ src/config.h
@@ -1,5 +1,5 @@
 /* ----------------------------------------------------------------------------
- * Copyright (c) 2014 Sergey Arnautov, Diogo Behrens
+ * Copyright (c) 2014,2015 Sergey Arnautov, Diogo Behrens
  * Distributed under the MIT license. See accompanying file LICENSE.
  * ------------------------------------------------------------------------- */
 
@@ -9,9 +9,16 @@
 #define ABUF_MAX_CONFLICTS 8000
 
 #define OBUF_SIZE 10     // at most 10 output messages per traversal
-#define COW_SIZE  40000    // at most 100 writes per traversal
+#define COW_SIZE  128    // at most 128 writes per traversal
 #define TBIN_SIZE 10000     // at most 10 frees per traversal
 #define TALLOC_MAX_ALLOCS 20000
+
+/* abuf and cow data structures are automatically reallocated if their
+ * capacity is reached. Uncomment the next lines to disable automatic
+ * reallocation. */
+//#define ABUF_DISABLE_REALLOC
+//#define COW_DISABLE_REALLOC
+
 
 #ifndef SEI_MT
 /* provide wrappers for system calls */
